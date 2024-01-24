965e3688e9e1c59c85425b64d710a6c2b271e7b1
unknown <silvanamorachaves@gmail.com>
2024-01-24 08:05:22 -0500

    instalacion npm mongo db node driver y connect

--- connect.js
+++ connect.js
@@ -1,10 +1,24 @@
+const { MongoClient } = require('mongodb');
 const config = require('./config');
 
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
-
-async function connect() {
-  // TODO: Database Connection
+const options = {
+  connectTimeoutMS: 3000,
+  socketTimeoutMS: 3000,
+  serverSelectionTimeoutMS: 3000,
+};
+const client = new MongoClient(config.dbUrl, options);
+function connect() {
+  try {
+    //await client.connect();
+    const db = client.db("burger_queen"); // Reemplaza <NOMBRE_DB> por el nombre del db
+    console.log("conectado")
+    return db;
+    } catch (error) {
+    console.log(error)//
+  }
 }
+
 
 module.exports = { connect };
--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const authMiddleware = require('./middleware/auth'); // Importamos un middleware
 const errorHandler = require('./middleware/error'); // importamos un middleware para manejar errores que ocurran durante la ejecución de la aplicació
 const routes = require('./routes'); // Importamos las rutas que definen las diferentes URL que la aplicación puede manejar y la lógica asociada a ellas.
 const pkg = require('./package.json'); // Importamos información del archivo package.json del proyecto, que contiene metadatos como su nombre, versión y dependencias.
+const {connect} = require ('./connect');
 
 const { port, secret } = config; // Extrae el port (número de puerto) y el secret (probablemente usado para autenticación) del objeto de configuración importado.
 const app = express(); //  Crea una instancia de la aplicación Express, que actúa como el centro principal para manejar solicitudes y respuestas.
-
+const db = connect();
 app.set('config', config); // Hace que el objeto de configuración esté disponible en toda la aplicación
 app.set('pkg', pkg); // Hace que la información del paquete esté disponible en toda la aplicación usando
 
@@ -16,6 +17,7 @@ app.set('pkg', pkg); // Hace que la información del paquete esté disponible en
 app.use(express.urlencoded({ extended: false })); // Analiza los cuerpos de las solicitudes entrantes en formato application/x-www-form-urlencoded (comúnmente usado para enviar formularios).
 app.use(express.json()); // Analiza los cuerpos de las solicitudes entrantes en formato JSON (común para interacciones API).
 app.use(authMiddleware(secret)); //  Aplica el middleware de autenticación para proteger ciertas rutas, usando el secret para verificación.
+
 
 // Registrar rutas
 routes(app, (err) => {
